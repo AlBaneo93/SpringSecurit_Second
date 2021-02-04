b085f5b69e94383bfb2ee3147d574b5ea9d57bfe
AlBaneo93 <albaneo0724@gmail.com>
2021-02-05 00:32:37 +0900

    aa

--- front/src/components/home.js
+++ front/src/components/home.js
@@ -1,24 +1,13 @@
-import React, { Component } from 'react'
-import { Route } from 'react-router-dom'
-import Info from './info'
-import Signin from './Signin'
-import Signup from './Signup'
-import Denied from './denied'
-import Error from './error'
+import React, { Component } from "react";
 
 class Home extends Component {
-    render() {
-        return (
-            <div>
-                <h1> Home</h1 >
-                {/* path="/이동할 주소", component ={출력할컴포넌트} */}
-                <Route path="/info" component={Info} />
-                <Route path="/signin" component={Signin} />
-                <Route path="/signup" component={Signup} />
-                <Route path="/denied" component={Denied} />
-                <Route path="/error" component={Error} />
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <h1>Hello World</h1>
+      </div>
+    );
+  }
 }
-export default Home
+
+export default Home;
--- front/src/index.js
+++ front/src/index.js
@@ -1,17 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import Home from './components/home';
-import ReportWebVitals from './reportWebVitals'
-
-
-import { Hot } from 'react-hot-loader/root'
-
-const Hot = Hot(Home);
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
 
 ReactDOM.render(
-  <Hot />,
-  document.getElementById('root')
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
 );
-
-ReportWebVitals();
